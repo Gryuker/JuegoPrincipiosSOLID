132b12f542c430b6bb36bfe867464bdc98fa037f
Gryuker <eskeleto1958@outlook.es>
2023-12-01 21:32:26 -0300

    Ultimos arreglos

---
+++ Assets/Scripts/Player/Inputs.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Inputs : MonoBehaviour{
+    public float yAxis;
+    public float xAxis;
+}
